8e633fa2e83783267661f324649b195349e38062
Stefan Hiob <stefan.hiob@outlook.de>
2023-02-03 23:13:08 +0100

    chore: add function lessions

--- functions/script.js
+++ functions/script.js
@@ -26,18 +26,64 @@ console.log(grossPrice());
   console.log(grossPrice2());
   */
 
+function grossPrice2(a, b) {
+  a = 40;
+  b = 0.16;
+
+  return a * b + a;
+}
+console.log(grossPrice2());
+
+console.log("Ende Gross Price Function");
 // add positive number
 
-function addPositive(a, b) {
+function addPositiveTest1(a, b) {
   a = 5;
   b = 9;
   return a + b;
 }
-console.log(addPositive());
+console.log(addPositiveTest1());
 
-function addPositive2(a, b) {
+function addPositiveTest2(a, b) {
   a = 5;
   b = -9;
   return a + Math.abs(b);
 }
+console.log(addPositiveTest2());
+
+console.log("Test Ende");
+
+// result1 should be 5
+function addPositive(a, b) {
+  a = 2;
+  b = 3;
+  return a + b;
+}
+console.log(addPositive());
+
+// result1 should be 8
+
+function addPositive2(a, b) {
+  a = 3;
+  b = -5;
+  return a + Math.abs(b);
+}
 console.log(addPositive2());
+
+// result1 should be 9
+
+function addPositive3(a, b) {
+  a = -1;
+  b = -8;
+  return Math.abs(a + b);
+}
+console.log(addPositive3());
+
+/* auach möglich , für den zufällige Zahlenwerte
+function addPositive3(a, b) {
+  a = -1;
+  b = -8;
+  return Math.abs(a) + Math.abs(b);
+}
+console.log(addPositive3());
+*/
