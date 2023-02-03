1b8c4ce4c98369c5d8f4bddaea62e33a9712789a
Stefan Hiob <stefan.hiob@outlook.de>
2023-02-03 22:58:09 +0100

    restore files

---
+++ operators-and-data/script.js
@@ -0,0 +1,23 @@
+//01) Write a String that logs out your first name
+
+console.log("S" + "t" + "e" + "f" + "a" + "n");
+
+//02) Spot the problems & solve them
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn´t work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 === 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" != 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 2 received 22
+console.log("" + 2);
