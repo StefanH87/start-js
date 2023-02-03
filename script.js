460d0ec3ae11e3621dc31ee728d97bbe3470129b
Stefan Hiob <stefan.hiob@outlook.de>
2023-02-03 22:51:20 +0100

    chore: restore lessions

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("Hello World!");
+
+//hehahejekdheruzrjr
+/*
+
+console.log(1 + 1);
+console.log(6 + 8);
+console.log(8 / 2);
+console.log(39 * 2);
+console.log(40 + 12 / 4);
+console.log(4 / 40 + 12);
+
+a = 4;
+b = 5;
+c = 10;
+d = a + b;
+e = c * a;
+f = 20 - b / 3 + c;
+
+console.log(d);
+console.log(e);
+console.log(f.toFixed());
+
+console.log(c + f);
+console.log(c + f.toString());
+console.log("Mathe-Übung");
+console.log("Ende !");
+ */
+//Comparison Operators
+
+console.log(3 == 8);
+console.log(8 != 5);
+console.log(4 > 6);
+console.log(4 < 6);
+console.log(25 + 4 >= 20 + 8);
+console.log(4 < 6 != 6);
+console.log(8 >= 8);
+console.log(10 + 5 == 10 + 5);
+console.log("Frage");
+console.log("Ich hasse Mathe".length == 15);
