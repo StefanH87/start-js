383c12b0498a11f6aaa3352e4c6903ab1220b60b
Stefan Hiob <stefan.hiob@outlook.de>
2023-02-07 00:18:42 +0100

    chore: finish task- variables

--- variables/script.js
+++ variables/script.js
@@ -1,1 +1,36 @@
-console.log("Hallo!");
+/*---------04) Coding-Task: Variables--------*/
+/*----01) Spot the errors and fix them----*/
+console.log("01) Spot the errors and fix them");
+
+let userName = "Brad";
+userName = "Jenna";
+
+function getUserNameLength(name) {
+  let result = name.length;
+  return result;
+}
+/*----02) isString function--*/
+
+console.log(getUserNameLength(userName) > 4);
+// ^______________ Should log true
+
+console.log("02) isString function");
+
+const isString = function (value) {
+  return typeof value === "string";
+};
+
+console.log(isString("Hello"));
+// result should be true
+
+console.log(isString(3));
+// result should be false
+
+console.log(isString(undefined));
+// result should be false
+
+console.log(isString(""));
+// result should be true
+
+console.log(isString("John" + "Doe"));
+// result should be true
